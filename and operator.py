bfbb5998531a42cb025c179304538c72db7f7d6b
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-24 22:39:54 +0530

    Create and operator.py

---
+++ and operator.py
@@ -0,0 +1,6 @@
+my_age = 22
+insert = int(("enter your age"))
+if(user_age > my_age and user_age == my_age)
+print("user is elder than myage","user age is equal to my age")
+if(user_age < my_age and user_age == my_age)
+print("user is younger than myage ","user age is equal to my age")
