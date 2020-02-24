3d70e8e701d0018683b02597876d0e883209a971
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-24 20:51:52 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,4 @@
+first_name = input("what is your first name?")
+second_name = input("what is your second number?")
+third_name = input("what is your third name?")
+print("the initials are",first_name[0],second _name[0],third_name[0])
