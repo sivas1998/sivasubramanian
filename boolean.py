541197ebbf9e2f022f39287a9953dc50687c1e5b
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-24 22:08:33 +0530

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,8 @@
+myage = 22
+user_age = int(("enter your age"))
+if(user_age > myage)
+print("your are older than me")
+elif(user_age == myage)
+print(" you and my are same age)
+else(user_age < myage
+print("you are younger than me")
