ec2534886afd3ce25d34df20d71dea3625f445ce
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-27 14:07:25 +0530

    Create using float,str,int and if .py

---
+++ using float,str,int and if .py
@@ -0,0 +1,9 @@
+my_name = "siva"
+my_age = "22"
+my_percentage = "6.5"
+if (my_name == siva):
+print("name: %s","my_name")
+if (my_age == 22):
+print("age: %d","my_age")
+if (my_percentage = "6.5")
+print("percentage %f","my_percentage")
