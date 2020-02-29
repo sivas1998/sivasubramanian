32e867e1bd922c24eae70da1975645a8cb021e80
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-29 16:56:58 +0530

    Create eg for multiple function arguement.py

---
+++ eg for multiple function arguement.py
@@ -0,0 +1,12 @@
+def siva(a , b , c)
+pass
+def alex(a , b ,c)
+pass
+if siva(1,2,3,4) == 1:
+print("good")
+if siva(1,2,3,4,5) == 2:
+print("better")
+if alex(1,2,3,4 = 6) == true:
+print("great")
+if alex(1,2,3,4,5 = 7) == false:
+print("not bad")
