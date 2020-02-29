dede73efd59943f5470a8194e37bd0b1ce792ed9
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-29 16:44:02 +0530

    Create exception handlind.py

---
+++ exception handlind.py
@@ -0,0 +1,6 @@
+actor = {"name":"vijay","rank":"good"}
+def get_last_name():
+return actor["name"].split()[1]
+get_last_name()
+print("all exception are good")
+print("actor name is"%s" get_last_name())
