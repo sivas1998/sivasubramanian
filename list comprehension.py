ae85d39c1bc26e7ed4200cad72475931811522f5
sivas1998 <61420610+sivas1998@users.noreply.github.com>
2020-02-29 17:05:50 +0530

    Create list comprehension.py

---
+++ list comprehension.py
@@ -0,0 +1,8 @@
+sentence = " i love myself and my name is siva"
+words = sentence . split()
+words_length = []
+for words in words:
+if word ! = "i":
+word_length.append(len(word))
+print(word)
+print(words_length)
